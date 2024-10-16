0d4425e2292eecb94e1af4c25a3f96480b8229f7
Peva Blanchard <pblanchard@kleis.ch>
2024-10-16 11:55:19 +0200

    fix formula

--- lcaac-cli.rb
+++ lcaac-cli.rb
@@ -1,11 +1,11 @@
 class LcaacCli < Formula
   desc "Command-line interface tool for LCAAC"
   homepage "https://github.com/kleis-technology/lcaac"
-  url "https://github.com/kleis-technology/lcaac/releases/download/v1.7.9/lcaac-cli-v1.7.9.tar.gz"
-  sha256 "3ad9c9b5871c5a9c907f1e9834614e58ef57e42aaa735c6fb9bc7ca4f8df09ea"
-  version "1.7.9" # Replace with the appropriate version
+  url "https://github.com/kleis-technology/lcaac/releases/download/v1.7.10/lcaac-cli-v1.7.10.tar.gz"
+  sha256 "8c273153ef26b339650ef7c042158cb8f7cdf430768d288cd6a306b2f8b4bb3a"
+  version "1.7.10" # Replace with the appropriate version
 
-  depends_on "openjdk@17" # Use the Java version that your application requires
+  depends_on "temurin@17" # Use the Java version that your application requires
 
   def install
     # Install all files into the libexec directory
@@ -16,8 +16,8 @@ class LcaacCli < Formula
   end
 
   test do
-    # Test that the CLI returns the expected help message or version output
-    assert_match "Usage", shell_output("#{bin}/lcaac --help")
+    # Test that the command runs successfully and exits with status 0
+    system "#{bin}/lcaac", "version"
   end
 end
 
